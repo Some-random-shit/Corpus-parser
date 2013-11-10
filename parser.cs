233a4ca12f996de216cf808951cc20671ca9b7e3
imilka <pkzl22@gmail.com>
2013-11-11 01:51:25 +0400

    Trying to fix indenting again (wtf studio?)

--- parser.cs
+++ parser.cs
@@ -11,47 +11,47 @@ namespace ConsoleApplication1
     {
         private Dictionary<int, sentence> sentenceMap;
 
-		private static String XML_NODE_WORD = "W";
-		private static String XML_NODE_SENTENCE = "S";
-		private static String XML_ROOT_NODE = "_root";
+        private static String XML_NODE_WORD = "W";
+        private static String XML_NODE_SENTENCE = "S";
+        private static String XML_ROOT_NODE = "_root";
 
-		private static String SENTENCE_ATTR_ID = "ID";
+        private static String SENTENCE_ATTR_ID = "ID";
 
-		private static String WORD_ATTR_DOM = "DOM";
-		private static String WORD_ATTR_FEAT = "FEAT";
-		private static String WORD_ATTR_ID = "ID";
-		private static String WORD_ATTR_LEMMA = "LEMMA";
-		private static String WORD_ATTR_LINK = "LINK";
+        private static String WORD_ATTR_DOM = "DOM";
+        private static String WORD_ATTR_FEAT = "FEAT";
+        private static String WORD_ATTR_ID = "ID";
+        private static String WORD_ATTR_LEMMA = "LEMMA";
+        private static String WORD_ATTR_LINK = "LINK";
 
         public void parse(string fileName)
         {
             XDocument doc =  XDocument.Load(fileName);
             sentenceMap = new Dictionary<int, sentence>();
-    
-			foreach (XElement sentence in doc.Root.Elements().Elements(XML_NODE_SENTENCE))
+
+            foreach (XElement sentence in doc.Root.Elements().Elements(XML_NODE_SENTENCE))
             {
-            	Dictionary<int, word> wordsMap = new Dictionary<int, word>();
+                Dictionary<int, word> wordsMap = new Dictionary<int, word>();
 
-				foreach (XElement word in sentence.Elements(XML_NODE_WORD))
+                foreach (XElement word in sentence.Elements(XML_NODE_WORD))
                 {
-					int dom = 0;
-					if (Convert.ToString((string) word.Attribute(WORD_ATTR_DOM).Value) != XML_ROOT_NODE) 
-						dom = Convert.ToInt32((string) element.Attribute(WORD_ATTR_DOM).Value);
+                    int dom = 0;
+                    if (Convert.ToString((string) word.Attribute(WORD_ATTR_DOM).Value) != XML_ROOT_NODE) 
+                        dom = Convert.ToInt32((string) element.Attribute(WORD_ATTR_DOM).Value);
 
-                  	word w = new word(dom,
-			        		(string) word.Attribute(WORD_ATTR_FEAT).Value,
-			        		Convert.ToInt32((string) word.Attribute(WORD_ATTR_ID).Value),
-			        		(string) word.Attribute(WORD_ATTR_LEMMA).Value,
-			        		(string) word.Attribute(WORD_ATTR_LINK).Value);
+                    word w = new word(dom,
+                            (string) word.Attribute(WORD_ATTR_FEAT).Value,
+                            Convert.ToInt32((string) word.Attribute(WORD_ATTR_ID).Value),
+                            (string) word.Attribute(WORD_ATTR_LEMMA).Value,
+                            (string) word.Attribute(WORD_ATTR_LINK).Value);
 
-              		wordsMap.Add(w.id, w);
+                    wordsMap.Add(w.id, w);
                 }
 
-            	sentence s = new sentence(Convert.ToInt32((string) sentence.Attribute(SENTENCE_ATTR_ID).Value),
-                		sentence.Value,
-                       	wordsMap);
+                sentence s = new sentence(Convert.ToInt32((string) sentence.Attribute(SENTENCE_ATTR_ID).Value),
+                        sentence.Value,
+                        wordsMap);
 
-            	sentenceMap.Add(s.id, s);
+                sentenceMap.Add(s.id, s);
             }
         }
     }

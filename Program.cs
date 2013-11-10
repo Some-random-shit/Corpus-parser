c098b103d281bd2b6b2bb39b035e4cc1e3474d3d
amorphousxd <amorphousxd@gmail.com>
2013-11-10 18:23:26 +0400

    123

--- Program.cs
+++ Program.cs
@@ -14,12 +14,15 @@ namespace ConsoleApplication1
         {
             string fileName = "C:/Minoru/Study/syntagrus24.09.2012/SynTagRus/2011/Alpinizm.tgt";
             XDocument doc = XDocument.Load(fileName);
-            foreach (XElement el in doc.Root.Elements().Elements("S"))
+            //выводим все предложения, el.value для элементов S будет последовательность всех элементов имен внутри предложения
+            foreach (XElement el in doc.Root.Elements().Elements("S")) //doc.Root.Elements это все наружные корневые элементы вложенные в бади(я так понял)
                 Console.WriteLine(el.Value);
                 Console.ReadKey();
+            //попробуем вывести атрибуты, выведем хотя бы айди предложений
             foreach (XElement element in doc.Root.Elements().Elements("S"))
                 Console.WriteLine(element.Attribute("ID"));
                 Console.ReadKey();
+            //попробуем пойти дальше и выведем атрибуты элементов элементов *фейспалм*
             foreach (XElement element in doc.Root.Elements().Elements("S").Elements("W")) //<-- ясн
                 Console.WriteLine(element.Attribute("LEMMA"));
                 Console.ReadKey();
